debe205280095167beebd022507b777d34095f6d
Ma-27 <mamhsl@163.com>
2020-10-25 23:11:38 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        //输入整形
+        int atk = scanner.nextInt();
+        //输入字符串
+        String name = scanner.nextLine();
+    }
+}
